b85b31fc27b6799e4b38025edfbc5faef1447833
Rhem <itsrhemiagain@gmail.com>
2024-07-09 23:03:06 +0800

    Update createGrid() for creating squares
    Added a .setProperty that will be used in css as a width for the squares

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 const gridContainer = document.querySelector("#grid-container");
 const changeGridBtn = document.querySelector("#change-grid-btn");
 
-let gridSize;
-
-changeGridBtn.addEventListener("click", () => {
-    gridSize = prompt("ENTER SIZE OF GRID THAT YOU WANT", 64);
-    createGrid(gridSize);
-})
 
 const createGrid = (size) => {
-    for(let i = 0; i < size; i++) {
+    gridContainer.innerHTML = "";
+    gridContainer.style.setProperty("--size", size);
+
+    for(let i = 0; i < size * size; i++) {
         let div = document.createElement("div");
         gridContainer.appendChild(div);
         div.classList.add("small-div");
-
     }
 
+    
 
 }
+
+changeGridBtn.addEventListener("click", () => {
+    let gridSize = parseInt(prompt("ENTER SIZE BETWEEN 16 AND 100", 64));
+
+    if (!gridSize || gridSize < 16 || gridSize > 100) {
+        alert("Please enter a number between 16 and 100");
+        return;
+    }
+    createGrid(gridSize);
+})
