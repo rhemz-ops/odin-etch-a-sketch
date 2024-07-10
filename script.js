5564819564d93899bf1151affdf5a4a19354b636
Rhem <itsrhemiagain@gmail.com>
2024-07-10 19:12:26 +0800

    Add new functions and event listener
    Create a new function that will apply depending on the mode

--- script.js
+++ script.js
@@ -1,6 +1,44 @@
+
+
 const gridContainer = document.querySelector("#grid-container");
 const changeGridBtn = document.querySelector("#change-grid-btn");
+const randomBtn = document.querySelector("#random-btn");
+const colorPicker = document.querySelector("#color-picker");
+const eraseBtn = document.querySelector("#erase-btn");
+const clearBtn = document.querySelector("#clear-btn");
 
+
+let isEraseMode = false;
+let isRandomMode = false;
+
+
+colorPicker.addEventListener("click", () => {
+    isEraseMode = false;
+})
+
+eraseBtn.addEventListener('click', () => {
+    eraseMode();
+})
+
+randomBtn.addEventListener("click", () => {
+    randomMode();
+})
+
+
+const eraseMode = () => {
+    isEraseMode = true;
+}
+
+const randomMode = () => {
+    isEraseMode = false;
+    isRandomMode = true;
+    let red = Math.floor(Math.random() * 256);
+    let green = Math.floor(Math.random() * 256);
+    let blue = Math.floor(Math.random() * 256);
+    return `rgb(${red}, ${green}, ${blue});`;
+}
+
+console.log(randomMode());
 
 const createGrid = (size) => {
     gridContainer.innerHTML = "";
@@ -10,13 +48,28 @@ const createGrid = (size) => {
         let div = document.createElement("div");
         gridContainer.appendChild(div);
         div.classList.add("small-div");
+        div.addEventListener('click', () => {
+            if(!isEraseMode && !isRandomMode) {
+                div.style.backgroundColor = colorPicker.value;
+            } else if (isEraseMode && !isRandomMode) {
+                div.style.backgroundColor = "transparent";
+            } else if (!isEraseMode && isRandomMode) {
+                div.style.backgroundColor = randomMode();
+            }
+            
+        })
+        clearBtn.addEventListener("click", () => {
+            const smallDivs = document.querySelectorAll(".small-div");
+            smallDivs.forEach(element => {
+                element.style.backgroundColor = "transparent";
+            })
+        })
     }
-
-    
 
 }
 
 changeGridBtn.addEventListener("click", () => {
+    isEraseMode = true;
     let gridSize = parseInt(prompt("ENTER SIZE BETWEEN 16 AND 100", 64));
 
     if (!gridSize || gridSize < 16 || gridSize > 100) {
@@ -25,3 +78,5 @@ changeGridBtn.addEventListener("click", () => {
     }
     createGrid(gridSize);
 })
+
+createGrid(16);
